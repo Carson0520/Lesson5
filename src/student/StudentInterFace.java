ed6d2a3f9dfb0ac1528dea60f13c01cc01b99b3a
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-09-28 12:12:51 -0400

    1/2 finished *

--- src/student/StudentInterFace.java
+++ src/student/StudentInterFace.java
@@ -11,41 +11,50 @@ public class StudentInterFace {
 
         String name;
         int score;
-        
+
         while (true) {
-        //Input first Studnet's data
-        System.out.print("Enter name for first student (min 1 letter); ");
-        name = scan.nextLine();
-        stu1.setName(name);
-        for (int i = 1; i <= 3; i++) {
-            System.out.format("Enter test score %d for %s --> ", i, stu1.getName());
-            score = scan.nextInt();
-            stu1.setScore(i, score);
-        }
-        String errormsg = stu1.validateData();
-        if (errormsg == null) {
-            break;
-        }
-        else {
-            System.out.println("errormsg");
-            scan.nextLine();
-        }
+            //Input first Studnet's data
+            System.out.print("Enter name for first student (min 1 letter); ");
+            name = scan.nextLine();
+            stu1.setName(name);
+            for (int i = 1; i <= 3; i++) {
+                System.out.format("Enter test score %d for %s --> ", i, stu1.getName());
+                score = scan.nextInt();
+                stu1.setScore(i, score);
+            }
+            String errormsg = stu1.validateData();
+            if (errormsg == null) {
+                break;
+            } else {
+                System.out.println(errormsg);
+                scan.nextLine();
+            }
         }
         scan.nextLine(); //clear scanner buffer
         System.out.println("\n-----------------------\n");
-        //input the second student's data
-        //Input the first student's data
-        System.out.println("Enter name for secondd students: ");
-        name = scan.nextLine();
-        stu2.setName(name);
-        for (int i = 1; i <= 3; i++) {
-            System.out.format("Enter test score %d for %s --> ", i, stu2.getName());
-            score = scan.nextInt();
-            stu2.setScore(i, score);            
+        while (true) { //input the second student's data
+            //Input the first student's data
+            {
+                System.out.println("Enter name for secondd students: ");
+            }
+            name = scan.nextLine();
+            stu2.setName(name);
+            for (int i = 1; i <= 3; i++) {
+                System.out.format("Enter test score %d for %s --> ", i, stu2.getName());
+                score = scan.nextInt();
+                stu2.setScore(i, score);
+            }
+            String errormsg = stu1.validateData();
+            if (errormsg == null) {
+                break;
+            } else {
+                System.out.println(errormsg);
+                scan.nextLine();
+            }
+            System.out.println("\n-----------------------\n");
+            //output the student's information
+            System.out.println("stu1");
+            System.out.println("stu2");
         }
-        System.out.println("\n-----------------------\n");
-        //output the student's information
-        System.out.println("stu1");
-        System.out.println("stu2");
     }
 }
