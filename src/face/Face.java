d67831609ebed27757b015630fc9408bcb995e28
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-09-29 12:36:02 -0400

    trying to finish face

--- src/face/Face.java
+++ src/face/Face.java
@@ -6,7 +6,8 @@ import javax.swing.JOptionPane;
 
 public class Face {
 
-    private int xPos, yPos, diameter;
+    private int xPos, yPos,newx,newy, diameter;
+    
     private Color color;
     private Graphics g;
 
@@ -31,23 +32,27 @@ public class Face {
         g.fillRect(xPos - 10, yPos - 10, diameter + 20, diameter + 20);
     }
 
-    private void btnsetActionPerformed(java.awt.event.ActionEvent evt) {
-        //enable all four control buttons (these should start out disabled)
-        btnmood.setEnabled(true);
-        btcolor.setEnabled(true);
-        btnmove.setEnabled(true);
-        btnresize.setEnabled(true);
-        f.erase();
-        f = new Face(facePanel.getGraphics(), 50, 50);
-        f.draw();
+    private void drawHead() {
+        g.setColor(color.red);
+        g.fillOval(xPos, xPos, diameter, diameter);
     }
 
-    private void btnsetActionPerformed(java.awt.event.ActionEvent evt) {
-        //TODO add your handling code here:
-        int newx = Integer.parseInt(JOptionPane.showInputDialog(this, "Enter  New X"));
-        int newy = Integer.parseInt(JOptionPane.showInputDialog(this, "Enter  New Y"));
-        f.erase();
-        f.move(newx, newy);
-        f.draw();
+    private void drawEyes() {
+        g.setColor(color.yellow);
+        g.fillOval(xPos + (int) (.2 * diameter), yPos + (int) (.2 * diameter), (int) (diameter * .25), (int) (diameter * .25));
+        g.fillOval(xPos + (int) (.55 * diameter), yPos + (int) (.2 * diameter), (int) (diameter * .25), (int) (diameter * .25));
+    }
+
+    private void drawMouth() {
+        int mx, my;
+        mx = xPos + (int) (.3 * diameter);
+        my = xPos + (int) (.8 * diameter);
+        g.setColor(color.black);
+        g.drawLine(mx, my, mx + (int) (.4 * diameter), my);
+    }
+
+    void move(int newx, int newy) {
+        newx=xPos;
+        newy=yPos;
     }
 }
