b289f469393c24f04ecc706457a091c688c53f97
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-10-14 13:39:20 -0400

    changed some things

--- src/Lucky7/Lucky74Real.java
+++ src/Lucky7/Lucky74Real.java
@@ -2,16 +2,15 @@ package Lucky7;
 
 import java.util.Scanner;
 
-
 public class Lucky74Real {
 
     public static void main(String[] args) {
         // TODO code application logic here
         int dollars = 0;
         int lastdollar = 0;
-        
+
         int rolls = 0;
-        int bestroll=0;
+        int bestroll = 0;
         Dice d1, d2;
 
         Scanner s = new Scanner(System.in);
@@ -20,38 +19,38 @@ public class Lucky74Real {
 
         System.out.println("How many dollars do you have? ");
         dollars = s.nextInt();
+        lastdollar = dollars;
 
         while (dollars != 0) {
-            
+
             d1.roll();
             d2.roll();
             int total = (d1.getNumber() + d2.getNumber());
-            
-            if (total==7){
-                dollars+=4;
+
+            if (total == 7) {
+                dollars += 4;
                 rolls++;
                 /*troubleshoot stuff
                 System.out.println("Roll Number: "+rolls);
                 System.out.println("Rolled a: "+total);
                 System.out.println("Money Left: "+dollars);
-                */
-                if  (lastdollar<dollars){
-                    lastdollar=dollars;
-                    bestroll=rolls;
+                 */
+                if (lastdollar < dollars) {
+                    lastdollar = dollars;
+                    bestroll = rolls;
                 }
-            }
-            else if (total !=7){
+            } else if (total != 7) {
                 dollars--;
                 rolls++;
                 /*troubleshoot stuff
                 System.out.println("Roll Number: "+rolls);
                 System.out.println("Rolled a: "+total);
                 System.out.println("Money Left: "+dollars);
-                */
+                 */
             }
             if (dollars == 0) {
                 System.out.println("You are broke after " + rolls + " rolls");
-                System.out.println("You should have quit after "+bestroll+" rolls when you had $" +lastdollar);
+                System.out.println("You should have quit after " + bestroll + " rolls when you had $" + lastdollar);
             }
         }
     }
--- src/face/Face.java
+++ src/face/Face.java
@@ -1,71 +1,95 @@
 package face;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Face {
+
     private int xpos, ypos, diameter;
     private Color color;
     private Graphics g;
     private boolean ishappy;
-    
-    public Face(Graphics g, int x, int y){
+    private boolean sad;
+
+    public Face(Graphics g, int x, int y) {
         xpos = x;
         ypos = y;
         this.g = g;
         diameter = 100;
         color = Color.red;
         ishappy = true;
-        
+
     }
-            int mx, my;
-    public void draw(){
+    int mx, my;
+
+    public void draw() {
         drawHead();
         drawEyes();
         drawMouth();
     }
-    
-    private void drawHead(){
+
+    private void drawHead() {
         g.setColor(color);
         g.fillOval(xpos, ypos, diameter, diameter);
     }
-    private void drawEyes(){
+
+    private void drawEyes() {
         g.setColor(Color.yellow);
-        g.fillOval(xpos + (int)(.2 * diameter), ypos + (int)(.2 * diameter), (int)(diameter * 0.25), (int)(diameter * 0.25));
-        g.fillOval(xpos + (int)(.55 * diameter), ypos + (int)(.2 * diameter), (int)(diameter * 0.25), (int)(diameter * 0.25));
+        g.fillOval(xpos + (int) (.2 * diameter), ypos + (int) (.2 * diameter), (int) (diameter * 0.25), (int) (diameter * 0.25));
+        g.fillOval(xpos + (int) (.55 * diameter), ypos + (int) (.2 * diameter), (int) (diameter * 0.25), (int) (diameter * 0.25));
     }
-    private void drawMouth(){
-      //  int mx, my;
-        mx = xpos + (int)(.3 * diameter);
-        my = xpos + (int)(.7 * diameter);
+
+    private void drawMouth() {
+        //  int mx, my;
+        mx = xpos + (int) (.3 * diameter);
+        my = xpos + (int) (.7 * diameter);
         g.setColor(Color.black);
-        g.drawLine(mx, my, mx + (int)(0.4 * diameter), my);
-        if(ishappy == true){
-            g.drawLine(mx - 10, my - 10, mx + (int)(0.4 * diameter) - 40, my);
-            g.drawLine(mx + 50, my - 10, mx + (int)(0.4 * diameter), my);
+        g.drawLine(mx, my, mx + (int) (0.4 * diameter), my);
+        if (ishappy == true) {
+            g.drawLine(mx - 10, my - 10, mx + (int) (0.4 * diameter) - 40, my);
+            g.drawLine(mx + 50, my - 10, mx + (int) (0.4 * diameter), my);
+        } else {
+            g.drawLine(mx - 10, my + 10, mx + (int) (0.4 * diameter) - 40, my);
+            g.drawLine(mx + 50, my + 10, mx + (int) (0.4 * diameter), my);
         }
-        else{
-            g.drawLine(mx - 10, my + 10, mx + (int)(0.4 * diameter) - 40, my);
-            g.drawLine(mx + 50, my + 10, mx + (int)(0.4 * diameter), my);
+        if (ishappy == false) {
+            g.drawLine(mx + 50, my - 10, mx + (int) (0.4 * diameter), my);
+            g.drawLine(mx - 10, my - 10, mx + (int) (0.4 * diameter) - 40, my);
+
+        } else {
+            g.drawLine(mx + 50, my + 10, mx + (int) (0.4 * diameter), my);
+            g.drawLine(mx - 10, my + 10, mx + (int) (0.4 * diameter) - 40, my);
+
         }
     }
-    public void toggleMood(){
-        ishappy = !ishappy;
+
+    public void toggleMood() {
+        if (ishappy = true) {
+            ishappy = false;
+        } else if (ishappy = false) {
+            ishappy = true;
+        }
     }
-    public void erase(){
+
+    public void erase() {
         g.setColor(Color.white);
         g.fillRect(xpos - 10, ypos - 10, diameter + 20, diameter + 20);
     }
-    public void move(int newx, int newy){
-       mx= xpos = newx;
-        
-      my=  ypos = newy;
+
+    public void move(int newx, int newy) {
+        mx = xpos = newx;
+
+        my = ypos = newy;
 
     }
-    public void resize(int newsize){
+
+    public void resize(int newsize) {
         diameter = newsize;
     }
-    public void color(Color newcolor){
+
+    public void color(Color newcolor) {
         color = newcolor;
     }
+
 }
